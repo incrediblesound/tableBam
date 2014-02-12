21a52e535dbe21f37851f3fb5d1c3ad7ac50d6ad
James H Edwards <james.howe.edwards@gmail.com>
2014-02-13 00:01:13 +0800

    Update tableBam.js
    eliminate global variables

--- tableBam.js
+++ tableBam.js
@@ -1,4 +1,4 @@
-var tableBam = function (id,name,data) {
+window.tableBam = function (id,name,data) {
   data = data.split('/');
   if(data[0][0] === 'b') {
       var table = $('<table id="'+name+'" border = '+data[0][1]+'>');
@@ -9,22 +9,18 @@ var tableBam = function (id,name,data) {
   for(var i=1;i<data.length;++i) {
     if(data[i][0] === 'h') {
       var a = data[i].split('.');
-      forEach(a, function(formH) {
+      a.splice(0,1);
+      a.forEach(function(formH) {
         $('#'+name).append('<th>'+formH+'</th>');
       });
     }
     else if(data[i][0] === 'd') {
       $('#'+name).append("<tr class='row"+i+"'>")
       var b = data[i].split('.');
-      forEach(b, function(formD) {
+      b.splice(0,1);
+      b.forEach(function(formD) {
         $('.row'+i).append('<td>'+formD+'</td>');
       });
     }
   }
-  return;
-};
-
-var forEach = function (array, fn) {
-  for(i=1;i<array.length;++i)
-    fn(array[i]);
-};
+}
